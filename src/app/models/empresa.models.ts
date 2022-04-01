17006abb497d1add4478240a5eeac7ade4b17424
Elmer Isaac Castillo González <ecastillo-2017029@kinal.edu.gt>
2022-03-31 19:16:42 -0600

    Actualizando

---
+++ src/app/models/empresa.models.ts
@@ -0,0 +1,15 @@
+export class Empresa{
+    constructor(
+        public _id:String,
+        public _nombre:String,
+        public _email:String,
+        public _password:String,
+        public _rol:String,
+        public _productos:String,
+        public _cantidad:Number,
+        public _tipoEmpresa:String
+
+    ){}
+
+
+}
